ee36a690edf33db208c5fc60d2953cac3db89e66
Sibel Özgöcer <ozgocersibel@gmail.com>
2025-01-31 21:32:04 +0300

    mesaj

--- GitTestApı/Program.cs
+++ GitTestApı/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+//gýt degýstýmý
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
